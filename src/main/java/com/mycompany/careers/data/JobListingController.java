cd06a057c91597f06913ae3228056aff7be50a68
Bharath Vissapragada <bvissapragada@salesforce.com>
2022-04-01 10:43:47 -0700

    Added new features

--- src/main/java/com/mycompany/careers/data/JobListingController.java
+++ src/main/java/com/mycompany/careers/data/JobListingController.java
@@ -1,6 +1,11 @@
 package com.mycompany.careers.data;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.MultipartFilter;
 
 @Controller
 @RequestMapping(path="/careers")
@@ -15,11 +22,44 @@ public class JobListingController {
 
   @Autowired
   private JobRepository jobRepository;
+  @Autowired
+  private JobSeekerRepository jobSeekerRepository;
 
+  //Displays list of open jobs
   @GetMapping()
   public @ResponseBody Iterable<JobPost> getJobList(){
-    System.out.println("in default method");
-        return jobRepository.findAll();
+    String jobList="";
+    Iterator<JobPost> jobsList = jobRepository.findAll().iterator();
+
+    while(jobsList.hasNext()){
+      JobPost job = jobsList.next();
+      jobList.concat(job.getJobTitle()+"\t"+job.getLocation()+"\t"+job.getJobDescription());
+    }
+    //return new ResponseEntity<>(jobList, HttpStatus.OK);
+    return jobRepository.findAll();
+
+  }
+
+
+  //Store user Data
+  @PostMapping(path="/apply")
+  public @ResponseBody String addApplicant(@RequestParam String firstName,
+                                           @RequestParam String lastName,
+                                           @RequestParam String email, @RequestParam long jobId,
+                                           @RequestParam MultipartFile resume) throws IOException {
+
+    JobSeekerProfile jobSeeker = new JobSeekerProfile();
+    jobSeeker.setJobId(jobId);
+    jobSeeker.setEmail(email);
+    jobSeeker.setFirstName(firstName);
+    jobSeeker.setLastName(lastName);
+    jobSeeker.setEmail(email);
+    jobSeeker.setResume(resume.getBytes());
+
+
+    jobSeekerRepository.save(jobSeeker);
+    return "saved!";
+
   }
 
 }
---
+++ src/main/java/com/mycompany/careers/data/JobSeekerRepository.java
@@ -0,0 +1,7 @@
+package com.mycompany.careers.data;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface JobSeekerRepository extends CrudRepository<JobSeekerProfile,Integer>{
+}
